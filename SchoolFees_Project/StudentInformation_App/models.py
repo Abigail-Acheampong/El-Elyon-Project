819f274613d180a512f2f628a49c4b8a08b6141e
Abigail Owusu Acheampong <owusuacheamponga@gmail.com>
2025-04-10 23:34:39 +0000

    Final changes

--- SchoolFees_Project/StudentInformation_App/models.py
+++ SchoolFees_Project/StudentInformation_App/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from Fee_App.models import Fee
 
 class StudentRecords(models.Model):
     registered_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="registered_students")
     name = models.CharField(max_length=100)
     admission_number = models.IntegerField(unique=True)  # Ensures uniqueness
-    grade = models.CharField(max_length=50)
-    guardian_name = models.CharField(max_length=100)  # Added guardian name
+    grade = models.CharField(max_length=50, default="Unknown")
+    guardian_name = models.CharField(max_length=100, default="Unknown") # Added guardian name
     guardian_contact = models.CharField(max_length=10)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
